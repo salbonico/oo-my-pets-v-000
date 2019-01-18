58f663723a4bd844b3a37fa3191c6856ff095557
Steve Albonico <salbonico@gmail.com>
2019-01-18 20:24:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
   # code goes here
+
+@@all = []
+
+def self.all
+  @@all
+end
+
+def self.count
+  self.all.count
+end
+
+def self.reset_all
+  self.all.clear
+end
+
+attr_reader :species
+
+
+def initialize(species)
+  @species = species
+  @@all << self
+end
+
+def species
+ new_owner = Owner.new("human")
+ return new_owner.species
+end
+
+
+def say_species
+  puts self.species
+end
+
 end
